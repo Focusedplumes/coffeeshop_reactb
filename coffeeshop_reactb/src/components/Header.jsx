31350da8fd4e1c912493378267fbf610a799d405
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-05-28 21:46:08 -0500

    header image update

--- coffeeshop_reactb/src/components/Header.jsx
+++ coffeeshop_reactb/src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import HeaderArt from './images/headertest.jpg'
+import HeaderArt from './images/coffeeShopInterior.jpg'
 
 export default function Header() {
   return (
     <div className='row-6'>
       <div className="art">
-          <header className="w-25 p-3">
+          <header className="w-75 p-3">
             <img src={HeaderArt}  className="img-fluid"  alt="coffee header"/> 
           </header>
       </div>
