c8094773bb7440fbc01ca2f56d32c3b41ec3550b
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-05-30 22:22:00 -0500

    trying to center navbar text

--- coffeeshop_reactb/src/components/Navbar.jsx
+++ coffeeshop_reactb/src/components/Navbar.jsx
@@ -4,28 +4,29 @@ function Navbar() {
   return (
     <div>
         <div className="row">
-            <div className="col-6">
+            <div className="col-12">
+                
+                    <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+                        <div className="container-fluid align-items-center">
+                        <a className="navbar-brand" href="#">CoffeeShop</a>
+                          <div className='container align-items-center'>
+                                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span className="navbar-toggler-icon"></span>
+                                </button>
+                                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                    <div className="navbar-nav">
+                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                    <a className="nav-link" href="#">Menu</a>
+                                    <a className="nav-link" href="#">Shopping Cart</a>
+                                    <a className="nav-link" href="#">Contact</a>
+                                    </div>
+                                </div>
+                                </div>
+                          </div>
+                    </nav>
                 
             </div>
-            <div className="col-6">
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                <a className="navbar-brand" href="#">CoffeeShop</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                            <div className="navbar-nav">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            <a className="nav-link" href="#">Menu</a>
-                            <a className="nav-link" href="#">Shopping Cart</a>
-                            <a className="nav-link" href="#">Contact</a>
-                            </div>
-                        </div>
-                        </div>
-                    </nav>
-                </div>
-            </div>
+        </div>
             
     </div>
   )
