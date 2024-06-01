f682cbb59aca34abfcbb4e463aa396f4807764d6
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-05-31 22:56:29 -0500

    more navigation bar centering exploration

--- coffeeshop_reactb/src/components/Navbar.jsx
+++ coffeeshop_reactb/src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
+      <div class="container text-center">
         <div className="row">
-            <div className="col-12">
-                
-                    <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+            <div className="col">
+                    <nav className="navbar navbar-expand-lg bg-body-tertiary">
                         <div className="container-fluid">
                         <a className="navbar-brand" href="#">CoffeeShop</a>
                           <div className='container'>
@@ -14,7 +14,7 @@ function Navbar() {
                                     <span className="navbar-toggler-icon"></span>
                                 </button>
                                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                                    <div className="navbar-nav align-items-center">
+                                    <div className="navbar-nav">
                                     <a className="nav-link active" aria-current="page" href="#">Home</a>
                                     <a className="nav-link" href="#">Menu</a>
                                     <a className="nav-link" href="#">Shopping Cart</a>
@@ -27,7 +27,7 @@ function Navbar() {
                 
             </div>
         </div>
-            
+      </div>      
     </div>
   )
 }
