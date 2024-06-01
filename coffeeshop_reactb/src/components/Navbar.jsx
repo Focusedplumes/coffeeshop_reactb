1ed76bfc3c38e50f01af979395b3f1932258eccc
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-05-31 22:04:29 -0500

    navbar centering work

--- coffeeshop_reactb/src/components/Navbar.jsx
+++ coffeeshop_reactb/src/components/Navbar.jsx
@@ -7,14 +7,14 @@ function Navbar() {
             <div className="col-12">
                 
                     <nav className="navbar navbar-expand-lg bg-body-tertiary ">
-                        <div className="container-fluid align-items-center">
+                        <div className="container-fluid">
                         <a className="navbar-brand" href="#">CoffeeShop</a>
-                          <div className='container align-items-center'>
+                          <div className='container'>
                                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                                     <span className="navbar-toggler-icon"></span>
                                 </button>
                                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                                    <div className="navbar-nav">
+                                    <div className="navbar-nav align-items-center">
                                     <a className="nav-link active" aria-current="page" href="#">Home</a>
                                     <a className="nav-link" href="#">Menu</a>
                                     <a className="nav-link" href="#">Shopping Cart</a>
