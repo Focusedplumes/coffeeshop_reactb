32c8cda27260eeea81829f15bd909efb5ee79d6f
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-06-06 07:26:23 -0500

    weather app update

--- coffeeshop_reactb/src/App.jsx
+++ coffeeshop_reactb/src/App.jsx
@@ -14,51 +14,51 @@ const router = createBrowserRouter([
   }
 ])
 
-function App() {
-  const weatherApiKey = 'dc607689992b371fbab3d718fb9f6e3b';
-  const [userCity, setUserCity] = useState(null)
-  const [currentTemp, setCurrentTemp] = useState(null) 
+// function App() {
+//   const weatherApiKey = '';
+//   const [userCity, setUserCity] = useState(null)
+//   const [currentTemp, setCurrentTemp] = useState(null) 
 
- useEffect(() => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          console.log('position', position)
-          const { latitude, longitude } = position.coords;
-          // //Anchorage 61.2181, -149.9003
-          // getWeatherData(61.2181, -149.9003)
-          // // Death Valley 36.4614, -116.8656
-          // getWeatherData(36.4614, -116.8656)
-          // //browser
-          getWeatherData(latitude, longitude)
-        },
-        (error) => {
-          console.error("Error getting location:", error);
-        }
-      );
-    }
-  }, [])
+//  useEffect(() => {
+//     if ("geolocation" in navigator) {
+//       navigator.geolocation.getCurrentPosition(
+//         (position) => {
+//           console.log('position', position)
+//           const { latitude, longitude } = position.coords;
+//           // //Anchorage 61.2181, -149.9003
+//           // getWeatherData(61.2181, -149.9003)
+//           // // Death Valley 36.4614, -116.8656
+//           // getWeatherData(36.4614, -116.8656)
+//           // //browser
+//           getWeatherData(latitude, longitude)
+//         },
+//         (error) => {
+//           console.error("Error getting location:", error);
+//         }
+//       );
+//     }
+//   }, [])
 
-  async function getWeatherData(lat, lon){
-    console.log(lat)
-    console.log(lon)
-    let weatherAPIResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`);
-    console.log('weatherAPIResponse', weatherAPIResponse)
-    if(weatherAPIResponse.status != 200){
-      alert('could not get weather data')
-    }
-    let jsonData = await weatherAPIResponse.json()
-    console.log('jsonData', jsonData)
-    setUserCity(jsonData.name)
-    setCurrentTemp(Math.round(jsonData.main.temp))
-  }
+//   async function getWeatherData(lat, lon){
+//     console.log(lat)
+//     console.log(lon)
+//     let weatherAPIResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`);
+//     console.log('weatherAPIResponse', weatherAPIResponse)
+//     if(weatherAPIResponse.status != 200){
+//       alert('could not get weather data')
+//     }
+//     let jsonData = await weatherAPIResponse.json()
+//     console.log('jsonData', jsonData)
+//     setUserCity(jsonData.name)
+//     setCurrentTemp(Math.round(jsonData.main.temp))
+//   }
 
-  return (
-    <div className="App">
-      <Home />
-      <Weather currentTemp={currentTemp} userCity={userCity} />
-    </div>
-  );
-}
+//   return (
+//     <div className="App">
+//       <Home />
+//       <Weather currentTemp={currentTemp} userCity={userCity} />
+//     </div>
+//   );
+// }
 
 export default App;
