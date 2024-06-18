b8cfa37858af7704703305f179b64f5b299103de
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-06-18 01:04:27 -0500

    working on the button

--- coffeeshop_reactb/src/pages/MenuItems.jsx
+++ coffeeshop_reactb/src/pages/MenuItems.jsx
@@ -4,19 +4,19 @@ import MenuItem from './MenuItems'; // Import MenuItems
 import { fakeMenuItems } from '../fakeData'
 
 
-function CartItem({ menuItems, updateShoppingCart }){
-  return (
-    <div className='item-container'>
-      {menuItems.map(item => (
-        <MenuItem 
-          key={item.id} 
-          item={item} 
-          updateShoppingCart={updateShoppingCart} 
-        />
-      ))}
-    </div>
-  );
-}
+// function CartItem({ menuItems, updateShoppingCart }){
+//   return (
+//     <div className='item-container'>
+//       {menuItems.map(item => (
+//         <MenuItem 
+//           key={item.id} 
+//           item={item} 
+//           updateShoppingCart={updateShoppingCart} 
+//         />
+//       ))}
+//     </div>
+//   );
+// }
 
 export default function MenuItems({image, altText, price, item, updateShoppingCart}) {
   const addToCart = () => {
@@ -24,8 +24,10 @@ export default function MenuItems({image, altText, price, item, updateShoppingCa
   };
 
   return (
+    
     <div className='col-4'>
       {fakeMenuItems.map(item => (
+        
         <div className="card mb-3" style={{maxWidth: "270px"}}>
           <div className='row g-0'>
             <div className='col-md-4'>
@@ -44,21 +46,14 @@ export default function MenuItems({image, altText, price, item, updateShoppingCa
               </h5>
               <p className='card-text'><small className="text-body-secondary">{`$${price}`}</small></p>
               <button onClick={addToCart} className="btn btn-primary">Add To Cart</button>
-              {/* <a href="http://localhost:3000/ShoppingCart" className="btn btn-primary">Cart</a> // this needs to add item to shopping cart */}
               
+            
             </div>
           </div>
         </div>
-         ))},
-        {/* <div className="col-3">
-        Card 2  
-        </div>
-        <div className="col-3">
-        Card 3   
-        </div>
-        <div className="col-3">
-        Card 4   
-        </div> */}
+         ))}
+        
+       
 
     </div>
   )
